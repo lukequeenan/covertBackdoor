8850bcbc939f5a0161e26687bb33df7b124c9a09
Luke Queenan <lukequeenan@gmail.com>
2012-11-15 17:09:27 -0800

    Removed SSL stuff for now

--- src/functionality.h
+++ src/functionality.h
@@ -1,10 +1,6 @@
 #ifndef FUNCTIONALITY_H
 #define FUNCTIONALITY_H
 
-#include <openssl/crypto.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-
 #include "sharedLibrary.h"
 
 void executeSystemCall(char *command);
